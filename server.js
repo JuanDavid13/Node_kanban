ca090d830b8191e5ff06fabbe8510fb995b9f920
Juan David <juandavid.ramirez@iescampanillas.com>
2020-12-20 23:28:58 +0100

    heroku

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set('views',path.join(__dirname,'public'));
 app.set('view engine', 'ejs');
 
 //specify the port
-const PORT = process.env.PORT || 80;
+let PORT = process.env.PORT || 80;
 
 //Routes and middlewares
 app.use('/' ,router.router);
